adaf04a3e16a7cc41bfd9b3e110d6a13a44e684a
NebojsaStojakovic <nebojsastojakovicla@gmail.com>
2022-06-14 16:54:14 +0200

    folder structure

---
+++ watchme_frontend/src/App.js
@@ -0,0 +1,15 @@
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./container/Home";
+
+const App = () => {
+  const navigate = useNavigate();
+  return (
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='/*' element={<Home />} />
+    </Routes>
+  );
+};
+
+export default App;
